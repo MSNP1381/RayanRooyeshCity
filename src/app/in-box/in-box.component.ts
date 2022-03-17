aa13345a4e69706dced907db44eee347b48c0166
MSNP1381 <mohamadnematpoor@gmail.com>
2022-03-17 18:07:57 +0330

    Revert "toast added"
    This reverts commit c1f28c9c0b86ca325e40dfd20ba7864b7d8d7ed3.

--- src/app/in-box/in-box.component.ts
+++ src/app/in-box/in-box.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { Inbox } from '../services/data.service';
-import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-in-box',
   templateUrl: './in-box.component.html',
@@ -8,14 +7,8 @@ import { ToastController } from '@ionic/angular';
 })
 export class InBoxComponent implements OnInit {
   @Input() inBoxObj: Inbox ;
-  constructor(public toastController: ToastController) { }
-  async presentToast() {
-    const toast = await this.toastController.create({
-      message: 'ثبت شد',
-      duration: 2000
-    });
-    toast.present();
-  }
+  constructor() { }
+
   ngOnInit() {}
 
 }
